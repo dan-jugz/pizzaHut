2410cf7c6549e09987dfa2248d791eea4d56cb10
daniel njuguna <njugunadaniel364@gmail.com>
2019-06-17 08:24:39 +0300

    add background image to landingpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,26 @@
-var size = new Array['big','medium','small'];
-
 function fn1(){
     var select = document.getElementById("bigg");
-    alert(select.options[select.selectedIndex].value)};
+    alert(select.options[select.selectedIndex].value)
+};
 
-var crust = function addCrust(size){
-        return size*0.15
-   };
+
 
 function pizzaOrder(orderName,orderNumber,location,size){
     this.ordername = orderName;
     this.orderNumber = orderNumber;
     this.location = location;
-    this.size = size;
+    this.size = size[];
 };
+size = new Array['big','medium','small'];
+var orderName=document.getElementById("name").value;
+var orderNumber=document.getElementById("number").value;
+var location=document.getElementById("location").value;
+
 
 pizzaOrder.prototype.giveMePizza = function(size, crust, topping){
     if(size=='big'){price = 900}
     else if(size=='medium'){price = 700}
-    else(size==small){price = 500}
+    else(size=='small'){price = 500}
 
     return price
 }
@@ -35,3 +37,8 @@ var amount = function Price(price, crust, topping){
     return price+crust+topping;
 };
 
+$(document).ready(function(){
+    $('button').click(function(){
+       $('div').slideToggle(1000);
+    });
+});
