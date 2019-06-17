d9343925b0134b9edeb7b79f0b08cf33bf45fd13
daniel njuguna <njugunadaniel364@gmail.com>
2019-06-17 04:54:19 +0300

    add prototype and functions to calculate price

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,34 @@ function fn1(){
     var select = document.getElementById("bigg");
     alert(select.options[select.selectedIndex].value)};
 
+var crust = function addCrust(size){
+        return size*0.15
+   };
+
 function pizzaOrder(orderName,orderNumber,location,size){
     this.ordername = orderName;
     this.orderNumber = orderNumber;
     this.location = location;
     this.size = size;
+};
+
+pizzaOrder.prototype.giveMePizza = function(size, crust, topping){
+    if(size=='big'){price = 900}
+    else if(size=='medium'){price = 700}
+    else(size==small){price = 500}
+
+    return price
 }
+
+var crust = function addCrust(price){
+    return price*0.15
+};
+
+var topping = function addTopping(price){
+    return price*0.2
+};
+
+var amount = function Price(price, crust, topping){
+    return price+crust+topping;
+};
 
