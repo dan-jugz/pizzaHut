1a557efde72fe725d09fb9d0d97884c280bd9334
daniel njuguna <njugunadaniel364@gmail.com>
2019-06-17 17:22:58 +0300

    add computed pizza prices

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,68 @@
-function fn1(){
-    var select = document.getElementById("bigg");
-    alert(select.options[select.selectedIndex].value)
-};
+$(document).ready(function() {
+    $("#order-details").hide();
+    $("#deliver").hide();
+    
+function pizzaOrder(orderName,orderNumber,location,crust,topping,delivery){
+    
+    this.orderName=orderName;
+    this.orderNumber=orderNumber;
+    this.location=location;
+}
 
 let orderName=document.getElementById("name").value;
 let orderNumber=document.getElementById("number").value;
 let location=document.getElementById("location").value;
 
-function pizzaOrder(size, orderName,orderNumber,location,crust,topping,delivery){
-    this.size=size[];
-    this.orderName=orderName;
-    this.orderNumber=orderNumber;
-    this.location=location;
-    this.crust=crust;
-    this.topping=topping;
-    this.delivery=delivery;
 
-}
-
-function pizzaDeliver(orderName,orderNumber,location){
-    alert(orderName + "'s pizza will be delivered to " + location + "in 30 minutes. we will contact " + orderNumber)
+  
+function pizza(size,crust,topping){
+    this.pizzaSize=size;
+    this.pizzaCrust=crust;
+    this.pizzaTopping=topping;
+    this.pizzaPrice=0;
 };
 
-size = new Array['big','medium','small'];
-
-pizzaOrder.prototype.giveMePizza = function(size, crust, topping){
-    if(size=='big'){price = 900}
-    else if(size=='medium'){price = 700}
-    else(size=='small');{price = 500}
-
-    return price + crust + topping
-}
-
-var crust = function addCrust(price){
-    return price*0.15
+pizza.prototype.price(){
+    var grandTotal = [];
+    var pizzaSize = ["big", "medium", "small"];
+    var pizzaCrust = ["crispy", "stuffed", "gluten-free"];
+    var pizzaTopping = ["cheese", "brocoli", "mushroom", "black-olives","pineapple","ginger-and-kale"];}
+  /**pizza size to pizza sizes */
+  if (this.pizzaSize === pizzaSize[0]) {
+    pizzaPrice = +900;
+  }
+  else if (this.pizzaSize === pizzaSize[1]) {
+    pizzaPrice = +700;
+  }
+  else if (this.pizzaSize === pizzaSize[2]) {
+    pizzaPrice = +500;
+  }
+  /*pizza crust to pizza size to pizza price */
+  if (this.pizzaCrust === pizzaCrust[0]) {
+    pizzaPrice = +150;
+  }
+  else if (this.pizzaCrust === pizzaCrust[1]) {
+    pizzaPrice = +100;
+  }
+  else if (this.pizzaCrust === pizzaCrust[2]) {
+    pizzaPrice = +50;
+  }
+  /**pizza size to pizza toppings */
+  if (this.pizzaTopping === pizzaSize[0]) {
+    pizzaPrice = +150;
+  }
+  else if (this.pizzaTopping === pizzaSize[1]) {
+    pizzaPrice = +100;
+  }
+  else if (this.pizzaTopping === pizzaSize[2]) {
+    pizzaPrice = +50;
+  };
+  return this.pizzaPrice;
 };
 
-var topping = function addTopping(price){
-    return price*0.2
-};
+
+
+
 
 var amount = function Price(price, crust, topping){
     return price+crust+topping;
@@ -64,3 +88,29 @@ $(document).ready(function () {
         alert("Your order will be deliver to your location")
  
     })});
+
+    function fn1(){
+    var select = document.getElementById("bigg");
+    alert(select.options[select.selectedIndex].value)
+};
+
+function pizzaDeliver(orderName,orderNumber,location){
+    alert(orderName + "'s pizza will be delivered to " + location + "in 30 minutes. we will contact " + orderNumber)
+};
+
+
+pizzaOrder.prototype.giveMePizza = function(size, crust, topping){
+    if(size=='big'){price = 900}
+    else if(size=='medium'){price = 700}
+    else(size=='small');{price = 500}
+
+    return price + crust + topping
+}
+var crust = function addCrust(price){
+    return price*0.15
+};
+
+var topping = function addTopping(price){
+    return price*0.2
+};
+*/
